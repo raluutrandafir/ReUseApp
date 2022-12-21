7fdfcc26ef54f62bac2519f990c94751f52316dd
Fabian Nicola <fabian.nicola@soft-state.com>
2022-12-21 12:44:31 +0200

    Added new Product screen

---
+++ ReUse/src/screens/ProductScreen/ProductScreen.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export function ProductScreen() {
+    return <View style={{ width: '100%', height: '100%', backgroundColor: 'red' }}></View>;
+}
