d6ef6a5564abc2ee330419a1a7f020fe72f25426
Fabian Nicola <fabian.nicola@soft-state.com>
2023-01-04 17:04:40 +0200

    Added new Get Products

--- ReUse/src/screens/ProductScreen/ProductScreen.tsx
+++ ReUse/src/screens/ProductScreen/ProductScreen.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View, ScrollView } from 'react-native';
+import axios from 'axios';
+
 import { Product } from '../../components/Product';
 
 const mock = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+// 86.123.250.155
 export function ProductScreen() {
+    async function getProducts() {
+        // const respone = await axios.get('https://86.123.250.155:7289/api/Products/GetProducts');
+
+        const response = await fetch('http://86.123.250.155:7289/Products/GetProducts', {
+            method: 'GET',
+            headers: {
+                Accept: 'accept: text/plain',
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log(response);
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
     return (
         <View
             style={{
